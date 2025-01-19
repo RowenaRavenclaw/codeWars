8c9d3d42ac9312475a49a60dd06f36f3fcb70718
TeonaOdisharia <teona.developer@gmail.com>
2025-01-19 08:59:07 +0400

    5kyui

---
+++ 5kyu/extractTheDomainNameFromAUrl.mjs
@@ -0,0 +1,18 @@
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+// * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+// * url = "https://www.cnet.com"                -> domain name = cnet"
+
+function domainName(url) {
+  //your code here
+  if (url) {
+    return url.replace(/^(https?:\/\/)?(www.)?/, "").split(".")[0];
+  }
+  return "";
+}
+
+console.log(domainName("http://github.com/carbonfive/raygun"));
+console.log(domainName("http://www.zombie-bites.com"));
+console.log(domainName("https://www.cnet.com"));
+console.log(domainName("www.xakep.ru"));
